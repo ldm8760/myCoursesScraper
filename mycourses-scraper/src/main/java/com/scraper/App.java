469aaff5690672f2524e76bbbfb880da62aaf0fd
logan <ldm8760@rit.edu>
2025-10-30 08:58:55 -0400

    login class not fully functional (autofill is crashing)

--- mycourses-scraper/src/main/java/com/scraper/App.java
+++ mycourses-scraper/src/main/java/com/scraper/App.java
@@ -1,16 +1,9 @@
 package com.scraper;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException  
     {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-        driver.getTitle();
-        driver.quit();
-        System.out.println("Finished execution!");
+        Login.login();
     }
 }
